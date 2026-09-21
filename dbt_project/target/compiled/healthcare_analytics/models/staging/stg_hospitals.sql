5a0140b7e203293891ebdb570ef62627d2479448
srutikuchimanchi-05 <srutikuchimanchi@gmail.com>
2026-09-21 22:35:16 +0000

    Exclude dbt target/ build artifacts from version control

--- dbt_project/target/compiled/healthcare_analytics/models/staging/stg_hospitals.sql
+++
@@ -1,8 +0,0 @@
-select
-    facility_id,
-    facility_name,
-    state,
-    ownership_type,
-    cast(overall_rating as int64) as overall_rating
-from `healthcare-pipeline-509120.hospital_data.hospital_ratings`
-where overall_rating is not null
