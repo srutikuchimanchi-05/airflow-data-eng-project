5dabcf68905ba36acd2f1314d6049af993638b62
srutikuchimanchi-05 <srutikuchimanchi@gmail.com>
2026-09-21 22:33:43 +0000

    Add staging models for hospitals and readmissions

---
+++ dbt_project/target/compiled/healthcare_analytics/models/staging/stg_readmissions.sql
@@ -0,0 +1,10 @@
+select
+    facility_id,
+    measure_name,
+    excess_readmission_ratio,
+    predicted_readmission_rate,
+    expected_readmission_rate,
+    start_date,
+    end_date
+from `healthcare-pipeline-509120.hospital_data.hospital_readmissions`
+where excess_readmission_ratio is not null
---
+++ dbt_project/target/run/healthcare_analytics/models/staging/stg_hospitals.sql
@@ -0,0 +1,13 @@
+
+
+  create or replace view `healthcare-pipeline-509120`.`hospital_data`.`stg_hospitals`
+  OPTIONS()
+  as select
+    facility_id,
+    facility_name,
+    state,
+    ownership_type,
+    cast(overall_rating as int64) as overall_rating
+from `healthcare-pipeline-509120.hospital_data.hospital_ratings`
+where overall_rating is not null;
+
